76a7db85b67169818ecdf8090a8de49addae3985
flavien-hugs <geekactux@gmail.com>
2022-02-18 17:29:55 +0000

    filter product on category instance

--- shop/serializers.py
+++ shop/serializers.py
@@ -1,16 +1,29 @@
 # shop.serializers.py
 
-from rest_framework.serializers import ModelSerializer
-
+from rest_framework.serializers import(
+    ModelSerializer,
+    SerializerMethodField
+)
 from shop.models import Category, Product, Article
 
 
 class CategorySerializer(ModelSerializer):
 
+    products = SerializerMethodField
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = [
+            'id',
+            'name',
+            'products',
+            'date_created',
+            'date_updated',
+        ]
 
+    def get_products(self, instance):
+        queryset = instance.products.filter(active=True)
+        serializer = ProductSerializer(queryset, many=True)
+        return serializer.data
 
 class ProductSerializer(ModelSerializer):
 
