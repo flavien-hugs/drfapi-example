a1a5d9851246633eef8ea08cf1f443f32f455ec3
flavien-hugs <geekactux@gmail.com>
2022-02-18 15:28:34 +0000

    edit category serializer

---
+++ shop/serializers.py
@@ -0,0 +1,12 @@
+# shop.serializers.py
+
+from rest_framework.serializers import ModelSerializer
+
+from shop.models import Category
+
+
+class CategorySerializer(ModelSerializer):
+
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
