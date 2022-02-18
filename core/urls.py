1348d9252161f72dd4d61ac8ba6d7cbe6601c118
flavien-hugs <geekactux@gmail.com>
2022-02-18 15:20:07 +0000

    create project

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+"""
+core URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+https://docs.djangoproject.com/en/4.0/topics/http/urls/
+"""
+
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+]
